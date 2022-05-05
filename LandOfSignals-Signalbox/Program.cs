f9a7522c995411c0dfa7eea42eb67c268e3d1477
MarkenJaden <jjsch1410@gmail.com>
2022-05-05 22:44:24 +0200

    Remoed rerouting for https

--- LandOfSignals-Signalbox/Program.cs
+++ LandOfSignals-Signalbox/Program.cs
@@ -15,11 +15,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
-
-app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
